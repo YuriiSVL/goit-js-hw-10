dc8c600776fa1100ce1a7fe54691f7a381d9170c
Yurii Syvolapenko <svl.yurii@gmail.com>
2023-03-28 15:23:49 +0300

    removed dublcate functions

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -8,34 +8,3 @@ export function fetchCountries(name) {
     return response.json();
   });
 }
-
-export function renderListOfCountries(countries) {
-  const markup = countries
-    .map(country => {
-      {
-        return `<li class="country-list__item">
-  <img src="${country.flags.svg}" alt="flag" width="30" />${country.name.official}
-</li>`;
-      }
-    })
-    .join('');
-
-  refs.countryList.innerHTML = markup;
-}
-
-export function renderCountryInfo(countries) {
-  const country = countries[0];
-  const markup = `<div class="country-info__flag-title-wrapper">
-   <img src="${country.flags.svg}" alt="flag" width="50" />
-   <h1 class="country-info__title">${country.name.official}</h1>
-  </div>
-  <p><span class="country-info__point">Capital</span> : ${country.capital}</p>
-  <p><span class="country-info__point">Population</span> : ${
-    country.population
-  }</p>
-  <p><span class="country-info__point">Languages:</span> ${Object.values(
-    country.languages
-  )}</p>`;
-
-  refs.countryInfo.innerHTML = markup;
-}
